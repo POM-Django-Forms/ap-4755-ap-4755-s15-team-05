f2ef787d8854a03b29ff83af517d49edb2aa534a
Dmytro Novykov <dimacpoint@gmail.com>
2026-06-13 19:16:52 +0300

    added form for author, added new authors.html that combines authors_list and author_create; commented previous code to get it back if needed; to restore previous page uncomment commented code and delete code between '='

--- library/author/urls.py
+++ library/author/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.author_list, name="author_list"),
-    path("create/", views.author_create, name="author_create"),
+    # path("", views.author_list, name="author_list"),
+    path("", views.authors, name="authors"),
+    # path("create/", views.author_create, name="author_create"),
     path("delete/<int:author_id>/", views.author_delete, name="author_delete"),
 ]
--- library/author/views.py
+++ library/author/views.py
@@ -2,33 +2,32 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from .models import Author
-
+from .forms import AuthorForm
 
 def is_librarian(user):
     return user.is_authenticated and getattr(user, "role", 0) == 1
 
 
-def author_list(request):
-    authors = Author.get_all()
-    return render(request, "author/author_list.html", {"authors": authors})
-
-
-def author_create(request):
+def authors(request):
     if not is_librarian(request.user):
         return redirect("home")
-
+    
+    authors = Author.get_all()
+    
     if request.method == "POST":
-        name = request.POST.get("name")
-        surname = request.POST.get("surname")
-        patronymic = request.POST.get("patronymic")
-
-        if name and surname and patronymic:
-            Author.create(name=name, surname=surname, patronymic=patronymic)
-            return redirect("author_list")
-
-        messages.error(request, "All fields are required")
-
-    return render(request, "author/author_create.html")
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("authors")
+    else:
+        form = AuthorForm()
+    
+    context = {
+        "form": form,
+        "authors": authors
+    }
+    
+    return render(request, "author/authors.html", context)
 
 
 def author_delete(request, author_id):
@@ -41,7 +40,51 @@ def author_delete(request, author_id):
         related_manager = getattr(author, relation.get_accessor_name())
         if related_manager.exists():
             messages.error(request, "Author is attached to a book and cannot be deleted")
-            return redirect("author_list")
+            return redirect("authors")
 
     author.delete()
-    return redirect("author_list")
+    return redirect("authors")
+
+def author_list(request):
+    authors = Author.get_all()
+    return render(request, "author/author_list.html", {"authors": authors})
+
+
+# def author_create(request):
+#     if not is_librarian(request.user):
+#         return redirect("home")
+
+    # if request.method == "POST":
+    #     # name = request.POST.get("name")
+    #     # surname = request.POST.get("surname")
+    #     # patronymic = request.POST.get("patronymic")
+
+    #     # if name and surname and patronymic:
+    #     #     Author.create(name=name, surname=surname, patronymic=patronymic)
+    #     #     return redirect("author_list")
+
+    #     # messages.error(request, "All fields are required")
+    #     form = AuthorForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect("author_list")
+    # else:
+    #     form = AuthorForm()
+
+    # return render(request, "author/author_create.html", {'form': form})
+
+
+# def author_delete(request, author_id):
+#     if not is_librarian(request.user):
+#         return redirect("home")
+
+#     author = get_object_or_404(Author, id=author_id)
+
+#     for relation in author._meta.related_objects:
+#         related_manager = getattr(author, relation.get_accessor_name())
+#         if related_manager.exists():
+#             messages.error(request, "Author is attached to a book and cannot be deleted")
+#             return redirect("author_list")
+
+#     author.delete()
+#     return redirect("author_list")
